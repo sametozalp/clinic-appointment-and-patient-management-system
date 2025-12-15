6a6e84090c24e525946a9c8abef76554cd7f3f6e
Samet Özalp <93615071+sametozalp@users.noreply.github.com>
2025-12-15 20:02:59 +0300

    update audit and appointment rabbitmq configurations

--- audit-service/src/main/java/com/ozalp/audit_service/configs/RabbitMQConfig.java
+++ audit-service/src/main/java/com/ozalp/audit_service/configs/RabbitMQConfig.java
@@ -1,6 +1,8 @@
 package com.ozalp.audit_service.configs;
 
 import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,16 +12,31 @@ public class RabbitMQConfig {
 
     public static final String APPOINTMENT_EXCHANGE = "appointment.exchange";
     public static final String AUDIT_QUEUE = "audit.queue";
+    public static final String AUDIT_DLX = "audit.dlx";
+    public static final String AUDIT_DLQ = "audit.dlq";
 
     @Bean
     public TopicExchange appointmentExchange() {
-        return new TopicExchange(APPOINTMENT_EXCHANGE);
+        return new TopicExchange(APPOINTMENT_EXCHANGE, true, false);
+    }
+
+    @Bean
+    public DirectExchange auditDlx() {
+        return new DirectExchange(AUDIT_DLX);
+    }
+
+    @Bean
+    public Queue auditDlq() {
+        return QueueBuilder.durable(AUDIT_DLQ).build();
     }
 
     @Bean
     public Queue auditQueue() {
         return QueueBuilder
                 .durable(AUDIT_QUEUE)
+                .withArgument("x-dead-letter-exchange", AUDIT_DLX)
+                .withArgument("x-dead-letter-routing-key", AUDIT_DLQ)
+                .withArgument("x-max-delivery-count", 3) // 3 kere dene olmazsa dlq
                 .build();
     }
 
@@ -32,7 +49,30 @@ public class RabbitMQConfig {
     }
 
     @Bean
+    public Binding dlqBinding() {
+        return BindingBuilder
+                .bind(auditDlq())
+                .to(auditDlx())
+                .with(AUDIT_DLQ);
+    }
+
+    @Bean
     public Jackson2JsonMessageConverter jackson2JsonMessageConverter() {
         return new Jackson2JsonMessageConverter();
     }
+
+    @Bean
+    public SimpleRabbitListenerContainerFactory rabbitListenerContainerFactory(
+            ConnectionFactory connectionFactory) {
+
+        SimpleRabbitListenerContainerFactory factory =
+                new SimpleRabbitListenerContainerFactory();
+
+        factory.setConnectionFactory(connectionFactory);
+        factory.setAcknowledgeMode(AcknowledgeMode.MANUAL);
+        factory.setDefaultRequeueRejected(false);
+
+        return factory;
+    }
+
 }
--- audit-service/src/main/java/com/ozalp/audit_service/consumer/AuditEventConsumer.java
+++ audit-service/src/main/java/com/ozalp/audit_service/consumer/AuditEventConsumer.java
@@ -4,13 +4,18 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ozalp.audit_service.entities.AuditLog;
 import com.ozalp.audit_service.message.AppointmentCreatedEvent;
 import com.ozalp.audit_service.repositories.AuditRepository;
+import com.rabbitmq.client.Channel;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.stereotype.Component;
 
+import java.io.IOException;
 import java.time.LocalDateTime;
+
+import static com.ozalp.audit_service.configs.RabbitMQConfig.AUDIT_QUEUE;
 
 @Component
 @AllArgsConstructor
@@ -21,16 +26,44 @@ public class AuditEventConsumer {
     private final ObjectMapper mapper;
     private final Jackson2JsonMessageConverter jackson2JsonMessageConverter;
 
-    @RabbitListener(queues = "audit.queue")
-    public void consume(AppointmentCreatedEvent event) throws Exception {
+//    @RabbitListener(queues = AUDIT_QUEUE)
+//    public void consume(AppointmentCreatedEvent event) throws Exception {
+//
+//        AuditLog logEntry = new AuditLog();
+//        logEntry.setEventType(event.getClass().getSimpleName());
+//        logEntry.setCreatedAt(LocalDateTime.now());
+//        logEntry.setPayload(mapper.writeValueAsString(event));
+//
+//        repository.save(logEntry);
+//
+//        log.info("AUDIT LOG SAVED");
+//    }
 
-        AuditLog logEntry = new AuditLog();
-        logEntry.setEventType(event.getClass().getSimpleName());
-        logEntry.setCreatedAt(LocalDateTime.now());
-        logEntry.setPayload(mapper.writeValueAsString(event));
+    @RabbitListener(queues = AUDIT_QUEUE, containerFactory = "rabbitListenerContainerFactory")
+    public void consume(AppointmentCreatedEvent event, Channel channel, Message message) throws IOException {
 
-        repository.save(logEntry);
+        try {
+            AuditLog log = new AuditLog();
+            log.setEventType("AppointmentCreated");
+            log.setPayload(event.toString());
+            log.setCreatedAt(LocalDateTime.now());
 
-        log.info("AUDIT LOG SAVED");
+            repository.save(log);
+
+            channel.basicAck(
+                    message.getMessageProperties().getDeliveryTag(),
+                    false
+            );
+
+        } catch (Exception ex) {
+
+            log.error("AUDIT FAILED", ex);
+
+            channel.basicNack(
+                    message.getMessageProperties().getDeliveryTag(),
+                    false,
+                    false // requeue = false → DLQ
+            );
+        }
     }
 }
