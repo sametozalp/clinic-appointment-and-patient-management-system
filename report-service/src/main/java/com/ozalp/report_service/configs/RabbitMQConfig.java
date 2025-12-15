47796ec4128b583de90adf9461114aa563f365c0
Samet Özalp <93615071+sametozalp@users.noreply.github.com>
2025-12-15 21:21:20 +0300

    update reporting rabbitmq config

--- report-service/src/main/java/com/ozalp/report_service/configs/RabbitMQConfig.java
+++ report-service/src/main/java/com/ozalp/report_service/configs/RabbitMQConfig.java
@@ -1,6 +1,8 @@
 package com.ozalp.report_service.configs;
 
 import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,6 +12,13 @@ public class RabbitMQConfig {
 
     public static final String APPOINTMENT_EXCHANGE = "appointment.exchange";
     public static final String REPORTING_QUEUE = "reporting.queue";
+    public static final String REPORT_DLX = "report.dlx";
+    public static final String REPORT_DLQ = "report.dlq";
+
+    @Bean
+    public DirectExchange reportDlx() {
+        return new DirectExchange(REPORT_DLX);
+    }
 
     @Bean
     public TopicExchange appointmentExchange() {
@@ -17,9 +26,16 @@ public class RabbitMQConfig {
     }
 
     @Bean
+    public Queue reportDlq() {
+        return QueueBuilder.durable(REPORT_DLQ).build();
+    }
+
+    @Bean
     public Queue reportingQueue() {
         return QueueBuilder
                 .durable(REPORTING_QUEUE)
+                .withArgument("x-dead-letter-exchange", REPORT_DLX)
+                .withArgument("x-dead-letter-routing-key", REPORT_DLQ)
                 .build();
     }
 
@@ -32,7 +48,31 @@ public class RabbitMQConfig {
     }
 
     @Bean
+    public Binding dlqBinding() {
+        return BindingBuilder
+                .bind(reportDlq())
+                .to(reportDlx())
+                .with(REPORT_DLQ);
+    }
+
+    @Bean
     public Jackson2JsonMessageConverter jackson2JsonMessageConverter() {
         return new Jackson2JsonMessageConverter();
     }
+
+    @Bean
+    public SimpleRabbitListenerContainerFactory rabbitListenerContainerFactory(
+            ConnectionFactory connectionFactory) {
+
+        SimpleRabbitListenerContainerFactory factory =
+                new SimpleRabbitListenerContainerFactory();
+
+        factory.setConnectionFactory(connectionFactory);
+        factory.setMessageConverter(jackson2JsonMessageConverter());
+        factory.setAcknowledgeMode(AcknowledgeMode.MANUAL);
+        factory.setDefaultRequeueRejected(false);
+
+        return factory;
+    }
+
 }
--- report-service/src/main/java/com/ozalp/report_service/consumers/ReportingConsumer.java
+++ report-service/src/main/java/com/ozalp/report_service/consumers/ReportingConsumer.java
@@ -3,9 +3,13 @@ package com.ozalp.report_service.consumers;
 import com.ozalp.report_service.entities.AppointmentReport;
 import com.ozalp.report_service.models.AppointmentCreatedEvent;
 import com.ozalp.report_service.repositories.AppointmentReportRepository;
+import com.rabbitmq.client.Channel;
 import lombok.RequiredArgsConstructor;
+import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
+
+import java.io.IOException;
 
 import static com.ozalp.report_service.configs.RabbitMQConfig.REPORTING_QUEUE;
 
@@ -15,15 +19,37 @@ public class ReportingConsumer {
 
     private final AppointmentReportRepository repository;
 
-    @RabbitListener(queues = REPORTING_QUEUE)
-    public void consume(AppointmentCreatedEvent event) {
+//    @RabbitListener(queues = REPORTING_QUEUE)
+//    public void consume(AppointmentCreatedEvent event) {
+//
+//        AppointmentReport r = new AppointmentReport();
+//        r.setAppointmentId(event.getAppointmentId());
+//        r.setDoctorId(event.getDoctorId());
+//        r.setDate(event.getStartTime().toLocalDate());
+//        r.setStatus("CREATED");
+//
+//        repository.save(r);
+//    }
 
-        AppointmentReport r = new AppointmentReport();
-        r.setAppointmentId(event.getAppointmentId());
-        r.setDoctorId(event.getDoctorId());
-        r.setDate(event.getStartTime().toLocalDate());
-        r.setStatus("CREATED");
+    @RabbitListener(queues = REPORTING_QUEUE, containerFactory = "rabbitListenerContainerFactory")
+    public void consume(AppointmentCreatedEvent event, Channel channel, Message message) throws IOException {
 
-        repository.save(r);
+        try {
+            AppointmentReport r = new AppointmentReport();
+            r.setAppointmentId(event.getAppointmentId());
+            r.setDoctorId(event.getDoctorId());
+            r.setDate(event.getStartTime().toLocalDate());
+            r.setStatus("CREATED");
+
+            repository.save(r);
+
+            channel.basicAck(message.getMessageProperties().getDeliveryTag(), false); //multiple: sadece benim mesajımı onayla
+
+        } catch (Exception e) {
+
+            System.out.println(e.getMessage());
+
+            channel.basicNack(message.getMessageProperties().getDeliveryTag(), false, false);
+        }
     }
 }
